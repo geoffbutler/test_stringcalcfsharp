ef6da9ab5df73b9b30a12653142f1bc0cbe64f9e
Geoff Butler <geoffbutler@users.noreply.github.com>
2017-02-19 14:06:17 +1300

    Add code

---
+++ src/StringCalcKata.Tests/Tests.fs
@@ -0,0 +1,113 @@
+﻿namespace StringCalcKata.Tests
+
+module ProgramShould = 
+    open System
+    open Program
+    open Xunit
+
+    [<Fact>]
+    let return_zero_for_empty_string() = 
+        let input = ""
+        let expected = 0
+
+        let result = Program.calc input
+
+        Assert.StrictEqual(expected, result)
+
+    [<Fact>]
+    let return_a_single_number() = 
+        let input = "1"
+        let expected = 1
+
+        let result = Program.calc input
+
+        Assert.StrictEqual(expected, result)
+
+
+    [<Fact>]
+    let add_two_numbers() = 
+        let input = "1,2"
+        let expected = 3
+
+        let result = Program.calc input
+
+        Assert.StrictEqual(expected, result)
+
+
+    [<Theory>]
+    [<InlineData("1,1,1", 3)>]
+    [<InlineData("1,2,3,4", 10)>]
+    let add_any_number_of_numbers(input : string, expected : int) = 
+        let result = Program.calc input
+
+        Assert.StrictEqual(expected, result)
+
+
+    [<Fact>]
+    let handle_new_line_instead_of_comma_for_delim() = 
+        let input = "\n1\n2\n3\n4\n5\n"
+        let expected = 15
+
+        let result = Program.calc input
+
+        Assert.StrictEqual(expected, result)
+
+
+    [<Fact>]
+    let handle_single_char_custom_delim() = 
+        let input = "//;\n1;2"
+        let expected = 3
+
+        let result = Program.calc input
+
+        Assert.StrictEqual(expected, result)
+
+           
+    [<Theory>]
+    [<InlineData("-1", "negatives not allowed: -1")>]
+    [<InlineData("//;\n-1;-2", "negatives not allowed: -1,-2")>]
+    let should_throw_when_supplied_negative_numbers((input : string), (expected : string)) =                
+        try
+            Program.calc input
+        with
+            | ex -> if expected.Equals(ex.Message) then 0 else reraise()
+
+
+    [<Theory>]    
+    [<InlineData("2,1001", 2)>]
+    [<InlineData("//;\n1000;2;1001;2000", 1002)>]
+    [<InlineData("1,1000", 1001)>]
+    let should_ignore_numbers_greater_than_1000((input : string), (expected : int)) =                        
+        let result = Program.calc input
+
+        Assert.StrictEqual(expected, result)
+
+
+    [<Theory>]        
+    [<InlineData("1,999", 1000)>]
+    [<InlineData("1,1000", 1001)>]
+    [<InlineData("//;\n1000;2;3;", 1005)>]
+    let should_not_ignore_numbers_less_than_equal_to_1000((input : string), (expected : int)) =                        
+        let result = Program.calc input
+
+        Assert.StrictEqual(expected, result)
+
+
+    [<Fact>]
+    let handle_multi_char_custom_delim() = 
+        let input = "//[***]\n1***2***3"
+        let expected = 6
+        
+        let result = Program.calc(input)
+
+        Assert.StrictEqual(expected, result)
+
+
+    [<Fact>]
+    let handle_multiple_multi_char_custom_delim() = 
+        let input = "//[***][%%%]\n1***2%%%3"
+        let expected = 6
+
+        let result = Program.calc(input)
+
+        Assert.StrictEqual(expected, result)
---
+++ src/StringCalcKata/Program.fs
@@ -0,0 +1,67 @@
+﻿module Program = 
+
+    open System    
+    open System.Text.RegularExpressions
+
+    let sub_zerostr_for_empty str = 
+        if String.IsNullOrWhiteSpace str then "0" else str
+
+    let get_single_char_delims(str : string) =                 
+        if str.StartsWith("//") then ([| str.Substring(2, 1) |], 2) else ([| "," ; "\n" |], 0) // (delims, delimLength)
+
+    let get_multi_char_delims(str : string) =         
+        let regex = new Regex(@"\[{1}[^\]\[]*\]{1}");
+        let delims = regex.Matches(str) |> Seq.cast<Match> |> Seq.map (fun m -> m.Value.TrimStart('[').TrimEnd(']')) |> Seq.toArray
+        let delimLength = (2 + (delims |> Seq.map (fun d -> 2 + d.Length) |> Seq.sum) + 1) // 2x char for \\; 2x cn 1x char for \n
+        (delims, delimLength)
+
+    let get_num_data(str : string, delimsLength : int) = 
+        if delimsLength > 0 then str.Substring(delimsLength) else str
+
+    let get_numstr_parts(str : string) =         
+        let delims, delimsLength = if str.StartsWith("//[") then get_multi_char_delims str else get_single_char_delims str        
+        let num_data = get_num_data (str, delimsLength)        
+        num_data.Split (delims, StringSplitOptions.RemoveEmptyEntries)
+
+    let parse_nums num_strs = 
+        num_strs |> Seq.map Int32.Parse |> Seq.toArray
+
+    let find_less_than_zero_nums int_nums = 
+        int_nums |> Seq.filter (fun n -> n < 0) |> Seq.toArray
+
+    let format_negatives_not_allowed_error_message(less_than_zero_nums : int[]) = 
+        if less_than_zero_nums.Length > 0 
+        then less_than_zero_nums |> Seq.map (fun n -> n.ToString ()) |> Seq.fold (fun a b -> (a + b + ",")) "" |> (fun s -> s.TrimEnd(',')) 
+        else ""
+
+    let get_invalid_num_details nums_to_consider = 
+        let less_than_zero_nums = find_less_than_zero_nums nums_to_consider
+        let negative_num_message = format_negatives_not_allowed_error_message less_than_zero_nums
+        (less_than_zero_nums, negative_num_message, nums_to_consider)
+
+    let assert_no_negatives (bad_nums : int[], bad_nums_message, nums_to_consider) = 
+        if bad_nums.Length > 0 then failwith ("negatives not allowed: " + bad_nums_message) else nums_to_consider
+
+    let remove_numbers_gt_1000 int_nums = 
+        int_nums |> Seq.filter (fun n -> n < 1001) |> Seq.toArray
+
+    let validate_nums int_nums =                         
+        (get_invalid_num_details <| remove_numbers_gt_1000 int_nums) |> assert_no_negatives
+
+    let sum nums = 
+        nums |> Seq.sum
+
+    let calc input = 
+        sub_zerostr_for_empty input
+        |> get_numstr_parts 
+        |> parse_nums
+        |> validate_nums
+        |> sum
+
+
+
+    [<EntryPoint>]
+    let main argv = 
+        printfn "Hello world"
+        System.Console.ReadKey() |> ignore
+        0
